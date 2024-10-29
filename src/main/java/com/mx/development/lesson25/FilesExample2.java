f4fd2594a3207cc6ccacacca8337536af5fba010
Jose Said Olano Garcia <josesaid@gmail.com>
2024-10-28 21:37:08 -0600

    Add files via upload

---
+++ src/main/java/com/mx/development/lesson25/FilesExample2.java
@@ -0,0 +1,59 @@
+package com.mx.development.lesson25;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class FilesExample2 {
+    public static void main(String[] args) throws IOException {
+
+        LocalDateTime diaConFecha = LocalDateTime.of(2030, 05, 05, 3, 33);
+        System.out.println("diaConFecha 1: " + diaConFecha);
+        diaConFecha = diaConFecha.plusYears(1).plusDays(-2).minusMonths(-1);
+        System.out.println("diaConFecha 2: " + diaConFecha);
+
+        //initialize Path object
+        LocalDateTime time = LocalDateTime.now();
+        System.out.println("year: " + time.getYear());
+        System.out.println("month: " + time.getMonth());
+        System.out.println("day: " + time.getDayOfMonth());
+
+        LocalDate hoy = LocalDate.now()
+                .plusDays(1)
+                //.plusMonths(1)
+                .plusYears(1);
+        //obtener el caracter separador de cada SO. / \
+
+        //C:\HomeReact
+        String pathWindows = "C:\\HomeReact\\";
+        String pathMac = "/users/josesaidolanogarcia/newFolder1234/";
+        String archivo = "said.txt";
+
+
+        //Path directorioConFechaHoy = Paths.get(pathWindows + hoy );
+        //Path directorioConFechaHoy = Paths.get(pathWindows + hoy );
+        Path directorioConFechaHoy2 = Path.of(pathWindows + hoy );
+        Path directoryPath = Paths.get(pathMac + time.toString());
+
+
+        //create file
+        try {
+            Path path1 = Files.createDirectories(directorioConFechaHoy2);
+            System.out.println(">>" + path1.getFileName());
+            Path createdFilePath = Files.createDirectories(directoryPath);
+            System.out.println("created: " + directoryPath.toString());
+            System.out.println(directoryPath + " - Is directory? " + Files.isDirectory(createdFilePath));
+
+            //Files.delete(directoryPath);
+            //System.out.println("deleted: " + directoryPath.toString());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
---
+++ src/main/java/com/mx/development/lesson25/PathExample.java
@@ -0,0 +1,37 @@
+package com.mx.development.lesson25;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+
+public class PathExample {
+    static private final String ARCHIVO = "/Users/josesaidolanogarcia/2024-10-28/lunes28.txt";
+
+    public static void main(String[] args) {
+        System.out.println("LocalDate.now(): " +  LocalDate.now());
+
+        Path path = Paths.get(ARCHIVO);
+        System.out.println(path.getParent());
+        System.out.println(path.getFileName());
+        System.out.println(path.getRoot());
+        System.out.println(path.normalize());
+        System.out.println(path.toAbsolutePath());
+
+        Path path1 = Paths.get("/users/DeleteMe/file01/algoMas/x");
+        Path path2 = Path.of("/users/DeleteMe/file02");
+        Path folder = Path.of("/users/DeleteMe/folder");
+
+        System.out.println("5 >>" + path1.getNameCount());
+        try{
+            System.out.println("file02 = index[2]: " + path2.getName(3));
+        }catch(IllegalArgumentException iae){
+            //System.err.println(iae.getMessage());
+            System.out.println("Error detectado y correctamente manejado....");
+        }
+        System.out.println("FOLDER: " + folder.toFile().isDirectory());
+
+        System.out.println(folder.toAbsolutePath());
+
+    }
+
+}
